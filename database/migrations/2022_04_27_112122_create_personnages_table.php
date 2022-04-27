8db44da6813d170b0f1b07325a326a27954d416d
Mohammad GONS SAIB <mohagonssaib@gmail.com>
2022-04-27 13:26:20 +0200

    first commit

---
+++ database/migrations/2022_04_27_112122_create_personnages_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personnages', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('charactername');
+            $table->string('photo');
+            $table->integer('age');
+            $table->string('power');
+            $table->date('dateofbirth');
+            $table->foreignId('movie_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personnages');
+    }
+};
